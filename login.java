2b8138c8e0af0b3a47bec9d9bd71e10b3838a58d
pallavi-123456 <singhpallavi16501@gmail.com>
2025-12-07 14:31:23 +0530

    Add files via upload

--- login.java
+++ login.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 
 import javax.swing.*;
 
@@ -101,12 +102,24 @@ public class login extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         try{
             if(e.getSource()==button1){
+                Connn c= new Connn();
+                String cardno=  textField2.getText();
+                String pin = new String(passwordField3.getPassword());
+                String q="select * from login where card_number = '"+cardno+"' and pin='"+pin+"'";
+                ResultSet resultSet = c.statement.executeQuery(q);
+                if(resultSet.next()){
+                setVisible(false);
+                new main_Class(pin);
+                }else {
+                    JOptionPane.showMessageDialog(null,"Incorrect Card Number or PIN");
+                }
 
             }else if(e.getSource()==button2){
             textField2.setText("");
             passwordField3.setText("");
             }else if (e.getSource()==button3){
-                
+                new Signup();
+                setVisible(false);
             }
         }catch(Exception E){
             E.printStackTrace();
